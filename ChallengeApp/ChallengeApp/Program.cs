80b2ac8ea2bfe6dd0b80de366905477841ed3894
TotalKate <kasia.kucal@gmail.com>
2023-06-28 00:03:48 +0200

    Zadanie domowe dzień 4

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string imię = "Ewa";
+string płeć = "Kobieta";
+var wiek = 33;
+
+if (wiek < 30)
+{
+    Console.WriteLine("Kobieta powyżej 30 lat");
+}
+else
+{
+    Console.WriteLine("Kobieta poniżej 30 lat");
+}
+
+if (imię == "Ewa")
+{
+    if (wiek > 30)
+    {
+        Console.WriteLine("Ewa, lat 33");
+    }
+    else
+    {
+        Console.WriteLine("Kobieta poniżej 30 lat");
+    }
+}
+
+if(płeć=="Kobieta")
+{
+    if (wiek<18)
+    { 
+        Console.WriteLine("Kobieta niepełnoletnia");
+    }
+    else
+    {
+        Console.WriteLine("Mężczyzna niepełnoletni");
+    }
+}
